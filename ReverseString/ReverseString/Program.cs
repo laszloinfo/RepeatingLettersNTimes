2d550f067a9ff25ea5c9c8def1ccfb7eda33d5a7
Kovacs.Alexandra.E <Kovacs.Alexandra.E@O16G07>
2020-01-27 12:57:02 +0100

    sadasd

---
+++ ReverseString/ReverseString/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("kérek egy mondatot");
+            string bemeno = Console.ReadLine();
+
+            Console.WriteLine($"A megfordított string:{Megfordit(bemeno)}");
+            Szoveg szov = new Szoveg(bemeno);
+            Console.WriteLine($"a megforditott szöveg: {szov.Megfordit()}");
+
+
+
+
+
+            Console.ReadKey(true);
+        }
+
+        static string Megfordit(string bemeno)
+        {
+            string kimeno = "";
+            for (int i = bemeno.Length -1; i >=  0; i--)
+            {
+                kimeno = String.Concat(kimeno, bemeno[i]);
+            }
+            return kimeno;
+        }
+}
+}
+
+
---
+++ abcleves/abcleves/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abcleves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kérem a szót: ");
+            string bemeno = Console.ReadLine();
+
+            Console.WriteLine($"a szó abc sorrendben: {Sorrendbe_1(bemeno)}");
+            Console.WriteLine($"a szó abc sorrendben: {Sorrendbe_2(bemeno)}");
+
+            Console.ReadKey(true);
+        }
+        static string Sorrendbe_1(string bemeno)
+        {
+            
+
+            char[] tomb = bemeno.ToCharArray();
+            Array.Sort(tomb);
+
+            return new string(tomb);
+
+          
+
+
+        }
+
+
+        static string Sorrendbe_2(string bemeno)
+        {
+
+
+            string kimeno = "{";
+           
+
+            for (int i = 0; i < bemeno.Length; i++)
+            {
+                for (int j = 0; j < kimeno.Length; j++)
+                {
+                    if (bemeno[i] <kimeno[j])
+                    {
+                        kimeno = String.Concat(bemeno.Substring(0,j), bemeno[i],kimeno.Substring(j));
+                        break;
+
+                    }
+                }
+            }
+
+ return kimeno;
+
+        }
+    }
+}
